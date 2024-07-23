fdc5097f327a053b2cfa9ab9a40f06ad810043ba
Thomas ARPIN <tpain45200@gmail.com>
2024-07-23 02:18:59 +0200

    ok

---
+++ components/data.ts
@@ -0,0 +1,39 @@
+export const data = {
+    "messe": {
+      "Accueil et ouverture de la célébration": {
+        "Entrée et mise en place des invités": "Entrée et mise en place des invités",
+        "Entrée des futurs époux et leur cortège": "Entrée des futurs époux et leur cortège",
+        "Parole d’accueil du prêtre": "Parole d’accueil du prêtre",
+        "Chant d’entrée": {
+          "Titre": "La bonté de Dieu / Ny Hatsaragnanao",
+          "Contenu": "Version française : La bonté de Dieu\nJe t'aime Seigneur, ton amour ne faillit jamais\nTous les jours, tu me tiens dans tes mains\nDès que je me réveille jusqu'à mon coucher\nJ’ ai vécu dans la bonté de Dieu\nRefrain :\nToute ma vie, tu as été bon\nToute ma vie, tu as été fidèle\nAvec le souffle qui est en moi\nJe chanterai la bonté de Dieu\nJ'aime ta voix, tu me conduis à travers le feu\nDans les nuits noires, tu restes auprès de moi\nJe te connais comme un père et comme un ami\nJe chanterai la bonté de Dieu\nRefrain :\nToute ma vie, tu as été bon\nToute ma vie, tu as été fidèle\nAvec le souffle qui est en moi\nJe chanterai la bonté de Dieu\nVersion malgache : Ny Hatsaragnanao\nTiako Anao tompo oh\nTsy mba nagnary zah ny famindraponao\nTagnana nao nihazogna fiaignako\nMandraigna vao mifoha zaho\nAlohany hoandriako\nHihira zaho , hihira ny hatsaragnanao\nRefrain :\nMatoky Anao zaho tamin’ny fiainako\nNaboakanao zaho fa tsara loatra Anao\nIsaky ny miaigna zaho\nJesosy oh\nHiraiko ny hatsaragnanao\nTiako maharegny feonao\nMitondra zaho agnaty ny ady\nNa agnaty aligny eh\nTsy miala marikitry zaho anao\nAiko anao io fa Babako eh\nNdrekindreky Anao Namako eh\nHihira zaho , hihira ny hatsaragnanao\nRefrain :\nMatoky Anao zaho tamin’ny fiainako\nNaboakanao zaho fa tsara loatra Anao\nIsaky ny miaigna zaho\nHihira zaho , hihira ny hatsaragnanao"
+        },
+        "Gloria": {
+          "Titre": "Gloria",
+          "Contenu": "Gloria, gloria\nIn excelsis Deo !\nGloria, gloria\nIn excelsis Deo !\nEt paix sur la terre aux hommes qu’il aime.\nNous te louons, nous te bénissons,\nNous t’adorons,\nNous te glorifions, nous te rendons grâce\nPour ton immense gloire,\nSeigneur Dieu, Roi du Ciel,\nDieu le Père tout-puissant.\nSeigneur, Fils unique, Jésus Christ,\nSeigneur Dieu, Agneau de Dieu\nLe Fils du Père,\nToi qui enlèves les péchés du monde,\nPrends pitié de nous ;\nToi qui enlèves les péchés du monde,\nReçois notre prière,\nToi qui es assis à la droite du Père,\nPrends pitié de nous.\nCar toi seul es Saint,\nToi seul es Seigneur,\nToi seul es le Très-Haut, Jésus Christ,\nAvec le Saint Esprit,\nDans la gloire de Dieu le Père.\nAmen."
+        },
+        "Prière du prêtre": "Prière du prètre"
+      },
+      "Liturgie de la Parole": {
+        "1ère lecture": {
+          "Titre": "1ère Lettre aux Corinthiens 12,31-13,8a",
+          "Contenu": "Frères, recherchez donc avec ardeur les dons les plus grands. Et maintenant, je vais vous indiquer le chemin par excellence. J’aurais beau parler toutes les langues des hommes et des anges, si je n’ai pas la charité, s’il me manque l’amour, je ne suis qu’un cuivre qui résonne, une cymbale retentissante.\nJ’aurais beau être prophète, avoir toute la science des mystères et toute la connaissance de Dieu, j’aurais beau avoir toute la foi jusqu’à transporter les montagnes, s’il me manque l’amour, je ne suis rien.\nJ’aurais beau distribuer toute ma fortune aux affamés, j’aurais beau me faire brûler vif, s’il me manque l’amour, cela ne me sert à rien.\nL’amour prend patience ; l’amour rend service ; l’amour ne jalouse pas ; il ne se vante pas, ne se gonfle pas d’orgueil ; il ne fait rien d’inconvenant ; il ne cherche pas son intérêt ; il ne s’emporte pas ; il n’entretient pas de rancune ; il ne se réjouit pas de ce qui est injuste, mais il trouve sa joie dans ce qui est vrai ; il supporte tout, il fait confiance en tout, il espère tout, il endure tout. L’amour ne passera jamais."
+        },
+        "Psaume": {
+          "Titre": "Psaume 144 (145),8-9. 10. 15. 17-18",
+          "Contenu": "Le Seigneur est tendresse et pitié,\nlent à la colère et plein d'amour ;\nla bonté du Seigneur est pour tous,\nsa tendresse, pour toutes ses œuvres.\nRefrain : Béni soit le Seigneur : son amour a fait pour nous des merveilles.\nQue tes œuvres, Seigneur, te rendent grâce\net que tes fidèles te bénissent !\nRefrain\nLes yeux sur toi, tous, ils espèrent :\ntu leur donnes la nourriture au temps voulu ;\nRefrain\nLe Seigneur est juste en toutes ses voies,\nfidèle en tout ce qu'il fait.\nIl est proche de ceux qui l'invoquent,\nde tous ceux qui l'invoquent en vérité.\nRefrain"
+        },
+        "Alléluia": {
+          "Titre": "Alléluia",
+          "Contenu": "Alléluia, Alléluia,\nAlléluia, Alléluia !"
+        },
+        "Évangile": {
+          "Titre": "Évangile selon saint Luc 6,31-38",
+          "Contenu": "En ce temps là, Jésus déclarait à ses disciples : « Ce que vous voulez que les hommes fassent pour vous, faites-le aussi pour eux. Si vous aimez ceux qui vous aiment, quelle reconnaissance méritez-vous ? Même les pécheurs aiment ceux qui les aiment. Si vous faites du bien à ceux qui vous en font autant, quelle reconnaissance méritez-vous ? Même les pécheurs en font autant. Si vous prêtez à ceux dont vous espérez recevoir en retour, quelle reconnaissance méritez-vous ? Même les pécheurs prêtent aux pécheurs pour qu’on leur rende l'équivalent. Au contraire aimez vos ennemis, faites du bien et prêtez sans rien espérer en retour. Alors votre récompense sera grande, et vous serez fils du Très-Haut, car lui, il est bon pour les ingrats et les méchants.\nSoyez miséricordieux comme votre Père est miséricordieux. Ne jugez pas, et vous ne serez pas jugés ; ne condamnez pas, et vous ne serez pas condamnés. Pardonnez, et vous serez pardonnés. Donnez, et l’on vous donnera : c’est une mesure bien pleine, tassée, secouée, débordante, qui sera versée dans le pan de votre vêtement ; car la mesure dont vous vous servez pour les autres servira de mesure aussi pour vous."
+        },
+        "Homélie": "Homélie",
+        "Morceau de musique au Violoncelle": "Morceau de musique au Violoncelle joué par Maïwenn"
+      }
+    }
+  }
+  
